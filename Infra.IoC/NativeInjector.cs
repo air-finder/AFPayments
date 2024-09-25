eed73949ec6dc9d4cc805e4d1436d3e275b98aa1
Bruno Silva <73912017+brunovbsilva@users.noreply.github.com>
2024-09-24 23:27:05 -0300

    Initial commit

---
+++ Infra.IoC/NativeInjector.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Application;
+using Domain.SeedWork.Notification;
+using Infra.Data;
+using Infra.Data.Repository;
+using Infra.Utils.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.IoC
+{
+    [ExcludeFromCodeCoverage]
+    public static class NativeInjector
+    {
+        public static void AddLocalHttpClients(this IServiceCollection services, IConfiguration configuration) {}
+
+        public static void AddLocalServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Services
+            services.AddScoped<INotification, Notification>();
+            services.AddSingleton<IContainer, ServiceProviderProxy>();
+            #endregion
+
+            #region Repositories
+            #endregion
+        }
+
+        public static void AddLocalUnitOfWork(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connString = Builders.BuildConnectionString(configuration);
+            services.AddDbContext<Context>(options => options.UseLazyLoadingProxies().UseSqlServer(connString));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
