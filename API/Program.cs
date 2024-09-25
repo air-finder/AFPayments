290638b3e9a7c8d1720cc518a5109e98f44a40ee
Bruno Silva <73912017+brunovbsilva@users.noreply.github.com>
2024-09-24 23:27:05 -0300

    Initialize development

---
+++ API/Program.cs
@@ -0,0 +1,103 @@
+using System.Text.Json.Serialization;
+using API.Middlewares;
+using Domain.SeedWork.Notification;
+using HealthChecks.UI.Client;
+using Infra.IoC;
+using Infra.Utils.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.IdentityModel.Tokens;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers()
+    .AddJsonOptions(x => x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
+builder.Services.Configure<AppSettings>(builder.Configuration.GetSection("App:Settings"));
+builder.Services.AddSwaggerGen(c =>
+{
+    c.EnableAnnotations();
+    c.SwaggerDoc("0.0.1",
+        new Microsoft.OpenApi.Models.OpenApiInfo
+        {
+            Title = "Template API",
+            Version = "0.0.1",
+            Description = "Template de API responsavel pelo dominio Air Finder",
+            Contact = new Microsoft.OpenApi.Models.OpenApiContact { Name = "Air Finder" }
+        });
+});
+
+#region Local Injections
+builder.Services.AddLocalServices(builder.Configuration);
+builder.Services.AddLocalHttpClients(builder.Configuration);
+builder.Services.AddLocalUnitOfWork(builder.Configuration);
+builder.Services.AddLocalHealthChecks(builder.Configuration);
+#endregion
+
+builder.Host.UseSerilog((hostingContext, loggerConfiguration) =>
+    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration)
+);
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddOptions();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin();
+        builder.AllowAnyHeader();
+        builder.AllowAnyMethod();
+    });
+});
+
+var key = Convert.FromBase64String(builder.Configuration.GetSection("App:Settings:Jwt:Secret").Value!);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+ServiceLocator.Initialize(app.Services.GetRequiredService<IContainer>());
+app.MapHealthChecks("health", new HealthCheckOptions { ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse });
+app.MapControllers();
+app.UseRouting();
+app.UseCors("AllowAllOrigins");
+app.UseAuthorization();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/0.0.1/swagger.json", "Template API"); });
+}
+
+app.UseMiddleware<ControllerMiddleware>();
+
+try
+{
+    Log.Information("[ApiTemplate] Starting the application...");
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Application failed to start");
+}
+finally
+{
+    Log.Information("[ApiTemplate] Finishing the application...");
+    Log.CloseAndFlush();
+}
+
